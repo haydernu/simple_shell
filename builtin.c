4309206a78c62de3791d76dffcb0b62e287b9767
haydernu <74455355+haydernu@users.noreply.github.com>
2022-12-19 15:36:34 +0300

    Update builtin.c

--- builtin.c
+++ builtin.c
@@ -82,7 +82,7 @@ int _mycd(info_t *info)
 /**
  * _myhelp - changes the current directory of the process
  * @info: Structure containing potential arguments. Used to maintain
- *          constant function prototype.
+ *  constant function prototype.
  *  Return: Always 0
  */
 int _myhelp(info_t *info)
